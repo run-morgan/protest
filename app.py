fc938ac43e1bdbe1f03df469a359f79e34687997
run-morgan <exivk1@naver.com>
2024-09-10 11:42:03 +0900

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello world"
+def index():
+    user_agent = request.headers.get('User-Agent')
+    is_android = 'Android' in user_agent
+    is_ios = 'iPhone' in user_agent or 'iPad' in user_agent
+    is_mobile = is_android or is_ios
+    return render_template("index.html",
+                           is_mobile=is_mobile, is_android=is_android, is_ios=is_ios)
 
 if __name__ == '__main__':
     app.run(debug=True)
